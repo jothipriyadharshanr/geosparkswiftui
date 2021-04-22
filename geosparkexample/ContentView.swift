b454d957f0bbb90c391d6a861b4e0b00a94dd1f9
jothipriyadharshanr <joe@geospark.co>
2021-04-22 09:22:21 +0530

    Initial Commit

---
+++ geosparkexample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  geosparkexample
+//
+//  Created by GeoSpark Mac #3 on 22/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ geosparkexample/geosparkexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  geosparkexampleApp.swift
+//  geosparkexample
+//
+//  Created by GeoSpark Mac #3 on 22/04/21.
+//
+
+import SwiftUI
+
+@main
+struct geosparkexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
